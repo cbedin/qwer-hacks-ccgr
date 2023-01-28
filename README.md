0703a553064168b6adf98e834d0ca645ed1c6198
Cooper Bedin <cooperbedin@s-149-142-229-137.resnet.ucla.edu>
2023-01-28 14:39:49 -0800

    updating requirements

--- README.md
+++ README.md
@@ -3,6 +3,6 @@ to develop:
 
 ```
 python3 -m venv .venv
-python3 -m .venv/bin/activate
+source .venv/bin/activate
 python3 -m pip install -r requirements.txt
 ```
--- app.py
+++ app.py
@@ -3,9 +3,9 @@ from twilio.twiml.messaging_response import MessagingResponse
 
 app = Flask(__name__)
 
+"""
 @app.route("/sms", methods=['GET', 'POST'])
 def sms_reply():
-    """Respond to incoming calls with a simple text message."""
     # Start our TwiML response
     resp = MessagingResponse()
 
@@ -16,3 +16,11 @@ def sms_reply():
 
 if __name__ == "__main__":
     app.run(debug=True)
+"""
+
+@app.route("/")
+def hello():
+  return "Hello World!"
+
+if __name__ == "__main__":
+  app.run()
